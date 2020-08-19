f7ec35bf39b70c894ff6db8febc0fb15c5169edf
mjallais <maeliss.jallais@inria.fr>
2020-08-19 15:39:15 +0200

    BUG: Error when installing the package using pip

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
 
-setup(name='compute_rtop',
+setup(name='compute_RTOP',
       version='0.1',
       description='Compute RTOP given a dMRI image and a mask',
       url='',
       author='Maëliss Jallais',
       author_email='maeliss.jallais@inria.fr',
       license='MIT',
-      packages=['compute_rtop'],
+      packages=['compute_RTOP'],
       install_requires=[
           'astropy',
           'nibabel',
           'dipy',
       ],
       scripts=[
-          'scripts/compute_rtop'
+          'scripts/compute_RTOP'
       ],
       zip_safe=True)
 
